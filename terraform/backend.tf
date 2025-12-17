095dfaea21d3175e52f3c29d01325300edf258eb
Corentin Lallement <corentinlallement5@gmail.com>
2025-12-17 10:35:33 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,7 @@
-
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "ec2/terraform.tfstate"
+    bucket = "myterraformstatebuckets3"
+    key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
 }
